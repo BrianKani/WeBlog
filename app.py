091c9bd4ebb55434a8a114248828b2b4cba3340f
BrianKani <kipsangkangogo26@gmail.com>
2023-11-02 22:02:26 +0300

    Update

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return 'Welcome to WEBlog'
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
